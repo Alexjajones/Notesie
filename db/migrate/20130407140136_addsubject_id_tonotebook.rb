abc94ccd89309c999bbd937cab1140011bb25501
AlexLaptop <alexjajones1@gmail.com>
2013-04-09 13:55:44 +0100

    Deleted un-needed migrations

--- db/migrate/20130407140136_addsubject_id_tonotebook.rb
+++
@@ -1,7 +0,0 @@
-class AddsubjectIdTonotebook < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-end
--- db/migrate/20130407140343_addsubject_id_tonotebooks.rb
+++
@@ -1,9 +0,0 @@
-class AddNotebookIdToTasks < ActiveRecord::Migration
-  def change
-    add_column :notebooks, :subject_id, :integer
-  end
-
-  def down
-    remove_column :notebooks, :subject_id, :integer
-  end
-end
